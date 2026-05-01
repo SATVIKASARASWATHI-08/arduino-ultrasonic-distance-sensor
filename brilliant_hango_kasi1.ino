c3140a31e2100432a03a5e77a34f70fda5d07602
SatvikaSaraswathi <satvikasaraswathi@gmail.com>
2026-05-01 11:16:23 +0530

    Add files via upload

---
+++ brilliant_hango_kasi1.ino
@@ -0,0 +1,25 @@
+// C++ code
+//
+int trig=3;
+int echo=2;
+float duration,distance;
+
+void setup()
+{
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2); 
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10); 
+  digitalWrite(trig, LOW);
+  
+  duration=pulseIn(echo,HIGH);
+  distance=duration*0.034/2;
+  Serial.println(distance);
+}
